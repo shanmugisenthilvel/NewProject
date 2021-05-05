079de2a5483384f1ed9ac6ad00c69a859407df12
SHANMUGI S <40590404+shanmugisenthilvel@users.noreply.github.com>
2021-05-05 13:04:57 +0530

    Add files via upload

---
+++ ConvertCurrencyServlet.java
@@ -0,0 +1,45 @@
+package com.currency;
+
+import java.io.IOException;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet implementation class ConvertCurrencyServlet
+ */
+public class ConvertCurrencyServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see GenericServlet#GenericServlet()
+     */
+    public ConvertCurrencyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
